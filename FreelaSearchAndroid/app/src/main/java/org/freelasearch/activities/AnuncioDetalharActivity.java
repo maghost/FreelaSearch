7aa62b4bf07e757b536433ee6c7b453121355082
Marques Rosa <msms@gft.com>
2016-06-02 17:15:36 -0300

    Ajustes no trabalho para algumas telas que deverão aparecer na print

--- FreelaSearchAndroid/app/src/main/java/org/freelasearch/activities/AnuncioDetalharActivity.java
+++ FreelaSearchAndroid/app/src/main/java/org/freelasearch/activities/AnuncioDetalharActivity.java
@@ -1,14 +1,17 @@
 package org.freelasearch.activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,27 +21,47 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import org.freelasearch.R;
+import org.freelasearch.dtos.DtoAnunciante;
 import org.freelasearch.dtos.DtoAnuncio;
+import org.freelasearch.dtos.DtoFreelancer;
+import org.freelasearch.dtos.DtoUsuario;
 import org.freelasearch.tasks.AsyncTaskListener;
+import org.freelasearch.tasks.impl.AsyncTaskAnunciante;
+import org.freelasearch.tasks.impl.AsyncTaskFreelancer;
+import org.freelasearch.tasks.impl.AsyncTaskListaAnunciante;
 import org.freelasearch.tasks.impl.AsyncTaskListaAnuncio;
+import org.freelasearch.tasks.impl.AsyncTaskListaFreelancer;
 import org.freelasearch.utils.EstadoUtils;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnuncioDetalharActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String PREF_NAME = "SignupActivityPreferences";
+    private SharedPreferences sharedpreferences;
 
     private DtoAnuncio anuncio;
     private Toolbar mToolbar;
     private Context context = this;
     private AsyncTaskListaAnuncio mAsyncTaskListaAnuncio;
 
+    private AsyncTaskListaAnunciante mAsyncTaskListaAnunciante;
+    private AsyncTaskListaFreelancer mAsyncTaskListaFreelancer;
+    private AsyncTaskAnunciante mAsyncTaskAnunciante;
+    private AsyncTaskFreelancer mAsyncTaskFreelancer;
+
+    private ProgressDialog progress;
+    private String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncio_detalhar);
+
+        sharedpreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
 
         if (getIntent() != null && getIntent().getExtras() != null) {
             if (getIntent().getExtras().getSerializable("anuncio") != null) {
@@ -67,6 +90,8 @@ public class AnuncioDetalharActivity extends AppCompatActivity implements View.O
             }
         });
 
+        email = sharedpreferences.getString("email", null);
+
         preencherTela();
     }
 
@@ -74,13 +99,18 @@ public class AnuncioDetalharActivity extends AppCompatActivity implements View.O
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_inscrever_se:
-                Toast.makeText(this, "Inscrever-se", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder alertDialogInscricao = new AlertDialog.Builder(this);
+                alertDialogInscricao.setTitle("Deseja inscrever-se?");
+                alertDialogInscricao.setMessage("Ao inscrever-se para a vaga será enviada uma notificação para o anunciante informando-o do seu interesse pelo anúncio.");
+                alertDialogInscricao.setPositiveButton("Ok", null);
+                alertDialogInscricao.setNegativeButton("Cancelar", null);
+                alertDialogInscricao.show();
                 break;
             case R.id.btn_logar_freelancer:
-                Toast.makeText(this, "Logar como Freelancer", Toast.LENGTH_SHORT).show();
+                selecionarPerfil("freelancer");
                 break;
             case R.id.btn_logar_anunciante:
-                Toast.makeText(this, "Logar como Anunciante", Toast.LENGTH_SHORT).show();
+                selecionarPerfil("anunciante");
                 break;
         }
     }
@@ -144,8 +174,6 @@ public class AnuncioDetalharActivity extends AppCompatActivity implements View.O
                 btnLogarAnunciante.setOnClickListener(this);
 
                 // Lógicas para exibir ou ocultar botões/funcionaldades
-
-                SharedPreferences sharedpreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
                 if (sharedpreferences.getInt("id", 0) != anuncio.getAnunciante().getUsuario().getId()) {
                     if (sharedpreferences.getInt("freelancer", 0) != 0) {
                         btnInscrever.setVisibility(View.VISIBLE);
@@ -175,6 +203,151 @@ public class AnuncioDetalharActivity extends AppCompatActivity implements View.O
                     fab.setVisibility(View.VISIBLE);
                 }
             }
+        }
+    }
+
+    //TODO: Tentar arrumar o texto "logado como ..." (NavigationView) quando faz a troca de perfil por essa tela
+    private void selecionarPerfil(String perfil) {
+        if (perfil.equals("anunciante")) {
+            mAsyncTaskListaAnunciante = new AsyncTaskListaAnunciante();
+            mAsyncTaskListaAnunciante.setAsyncTaskListener(new AsyncTaskListener() {
+                @Override
+                public void onPreExecute() {
+                    progress = new ProgressDialog(AnuncioDetalharActivity.this);
+                    progress.setMessage("Buscando perfil...");
+                    progress.setCanceledOnTouchOutside(false);
+                    progress.show();
+                }
+
+                @Override
+                public <T> void onComplete(T obj) {
+                    if (obj != null) {
+                        List<DtoAnunciante> dtoAnunciante = (List<DtoAnunciante>) obj;
+
+                        if (dtoAnunciante.size() == 1) {
+                            SharedPreferences.Editor editor = sharedpreferences.edit();
+                            editor.putInt("anunciante", dtoAnunciante.get(0).getId());
+                            editor.remove("freelancer");
+                            editor.commit();
+
+                            AnuncioDetalharActivity.this.recreate();
+                            progress.dismiss();
+                        } else {
+                            progress.setMessage("Você ainda não possui um perfil de Anunciante, criando perfil...");
+                            criarPerfil("anunciante");
+                        }
+                    } else {
+                        Log.e("FreelaSearch", "Falha ao selecionar o perfil do Anunciante");
+                        return;
+                    }
+                }
+
+                @Override
+                public void onError(String errorMsg) {
+                    Log.e("FreelaSearch", errorMsg);
+                }
+            });
+
+            Map<String, String> filtro = new HashMap<>();
+            filtro.put("email", sharedpreferences.getString("email", ""));
+            mAsyncTaskListaAnunciante.execute(filtro);
+        } else if (perfil.equals("freelancer")) {
+            mAsyncTaskListaFreelancer = new AsyncTaskListaFreelancer();
+            mAsyncTaskListaFreelancer.setAsyncTaskListener(new AsyncTaskListener() {
+                @Override
+                public void onPreExecute() {
+                    progress = new ProgressDialog(AnuncioDetalharActivity.this);
+                    progress.setMessage("Buscando perfil...");
+                    progress.setCanceledOnTouchOutside(false);
+                    progress.show();
+                }
+
+                @Override
+                public <T> void onComplete(T obj) {
+                    if (obj != null) {
+                        List<DtoFreelancer> dtoFreelancer = (List<DtoFreelancer>) obj;
+
+                        if (dtoFreelancer.size() == 1) {
+                            SharedPreferences.Editor editor = sharedpreferences.edit();
+                            editor.putInt("freelancer", dtoFreelancer.get(0).getId());
+                            editor.remove("anunciante");
+                            editor.commit();
+
+                            AnuncioDetalharActivity.this.recreate();
+                            progress.dismiss();
+                        } else {
+                            progress.setMessage("Você ainda não possui um perfil de Freelancer, criando perfil...");
+                            criarPerfil("freelancer");
+                        }
+                    } else {
+                        Log.e("FreelaSearch", "Falha ao selecionar o perfil do Freelancer");
+                        return;
+                    }
+                }
+
+                @Override
+                public void onError(String errorMsg) {
+                    Log.e("FreelaSearch", errorMsg);
+                }
+            });
+
+            Map<String, String> filtro = new HashMap<>();
+            filtro.put("email", sharedpreferences.getString("email", ""));
+            mAsyncTaskListaFreelancer.execute(filtro);
+        } else {
+            return;
+        }
+    }
+
+    private void criarPerfil(String perfil) {
+        if (perfil.equals("anunciante")) {
+            mAsyncTaskAnunciante = new AsyncTaskAnunciante();
+            mAsyncTaskAnunciante.setAsyncTaskListener(new AsyncTaskListener() {
+                @Override
+                public void onPreExecute() {
+                }
+
+                @Override
+                public <T> void onComplete(T obj) {
+                    progress.dismiss();
+                }
+
+                @Override
+                public void onError(String errorMsg) {
+                    Log.e("FreelaSearch", errorMsg);
+                }
+            });
+
+            DtoAnunciante dtoAnunciante = new DtoAnunciante();
+            DtoUsuario dtoUsuario = new DtoUsuario();
+            dtoUsuario.setId(sharedpreferences.getInt("id", 0));
+            dtoAnunciante.setUsuario(dtoUsuario);
+            mAsyncTaskAnunciante.execute(dtoAnunciante);
+        } else if (perfil.equals("freelancer")) {
+            mAsyncTaskFreelancer = new AsyncTaskFreelancer();
+            mAsyncTaskFreelancer.setAsyncTaskListener(new AsyncTaskListener() {
+                @Override
+                public void onPreExecute() {
+                }
+
+                @Override
+                public <T> void onComplete(T obj) {
+                    progress.dismiss();
+                }
+
+                @Override
+                public void onError(String errorMsg) {
+                    Log.e("FreelaSearch", errorMsg);
+                }
+            });
+
+            DtoFreelancer dtoFreelancer = new DtoFreelancer();
+            DtoUsuario dtoUsuario = new DtoUsuario();
+            dtoUsuario.setId(sharedpreferences.getInt("id", 0));
+            dtoFreelancer.setUsuario(dtoUsuario);
+            mAsyncTaskFreelancer.execute(dtoFreelancer);
+        } else {
+            return;
         }
     }
 
